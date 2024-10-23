5ef47fb2aaa8e55c04dc5c73fd49c451e8361e5b
crybaby-afk <toolarry55@gmail.com>
2024-10-23 17:43:33 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * (Number > 10) )+ 1;
+
+let num3 = 32;
+let num4 = 7;
+const mod = num3%num4
+
+const max = Math.max(1, 14, 11, 17, 20, 9 ,16,0.5 );
